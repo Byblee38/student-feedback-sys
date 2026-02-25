056926a4d9e5d7c5d3bb16822a43d63e68ee8953
Bayu Kresna Mukti <kresnamukti386@gmail.com>
2026-02-25 09:42:49 +0700

    feat : add sql file

---
+++ file_database/db-ubay.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 25, 2026 at 03:41 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db-ubay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(3) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `nama` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `nama`) VALUES
+(1, 'john', 'john', 'John Doe'),
+(2, 'bayu', 'bayu', 'Bayu Kresna'),
+(3, 'ubay', 'bismillah', 'Bayu#2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspirasi`
+--
+
+CREATE TABLE `aspirasi` (
+  `id_aspirasi` int(5) NOT NULL,
+  `status` enum('Menunggu','Proses','Selesai') NOT NULL,
+  `id_pelaporan` int(5) NOT NULL,
+  `feedback` int(5) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `aspirasi`
+--
+
+INSERT INTO `aspirasi` (`id_aspirasi`, `status`, `id_pelaporan`, `feedback`, `tanggal`) VALUES
+(1, 'Menunggu', 1, 12, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `input_aspirasi`
+--
+
+CREATE TABLE `input_aspirasi` (
+  `id_pelaporan` int(5) NOT NULL,
+  `nis` varchar(10) NOT NULL,
+  `id_kategori` int(5) NOT NULL,
+  `lokasi` varchar(50) NOT NULL,
+  `ket` varchar(50) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `input_aspirasi`
+--
+
+INSERT INTO `input_aspirasi` (`id_pelaporan`, `nis`, `id_kategori`, `lokasi`, `ket`, `tanggal`) VALUES
+(1, '0088662088', 1, 'Nesu', 'Keterangan Disini', '0000-00-00'),
+(2, '0088668098', 1, 'lab', 'Tidak Punya KELAS', '0000-00-00'),
+(3, '0088668098', 1, 'lab', 'mbg', '2026-02-05'),
+(4, '1', 3, 'lab', 'lapar', '2026-02-10'),
+(5, '1', 4, 'Lapangan', 'Tidak ada tempat parkir', '2026-02-23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(5) NOT NULL,
+  `nama_kategori` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(3, 'Pelayanan'),
+(4, 'Sarana Prasarana'),
+(6, 'Scam');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nis` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `jenis_kelamin` enum('Laki-Laki','Perempuan') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nis`, `nama`, `kelas`, `jenis_kelamin`) VALUES
+('0088668098', 'Bayu Kresna', 'XII RPL 2', 'Laki-Laki');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  ADD PRIMARY KEY (`id_aspirasi`);
+
+--
+-- Indexes for table `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  ADD PRIMARY KEY (`id_pelaporan`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  MODIFY `id_aspirasi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  MODIFY `id_pelaporan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
